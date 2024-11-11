131e65290089bdb9ed34d9706788393987c98c5c
Vitor Fraifer <vitorfraifer10@gmail.com>
2024-11-11 10:06:21 -0300

    feat: edição de dados das anuidades e dos associados + ajustes no fechamento de modais

--- index.php
+++ index.php
@@ -2,10 +2,9 @@
     require "conexao_bd.php";
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Verifique qual formulário foi submetido
         $formulario = $_POST['formulario'] ?? '';
     
-        // Processa o formulário de cadastro de usuário
+        // Condicional para verificar qual formulário foi submetido
         if ($formulario === 'cadastro-associado') {
             $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
             $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
@@ -31,7 +30,6 @@
             $ano = filter_input(INPUT_POST, 'ano', FILTER_SANITIZE_NUMBER_INT);
             $valor = filter_input(INPUT_POST, 'valor', FILTER_SANITIZE_NUMBER_INT);
             
-            // Insira a mensagem no banco
             try {
                 $sql = 'INSERT INTO "TecnoTech".anuidades (ano, valor) VALUES (:ano, :valor)';
                 $stmt = $pdo->prepare($sql);
@@ -57,7 +55,9 @@
             } catch (PDOException $e) {
                 echo "Erro ao excluir associado: " . $e->getMessage();
             }
-        } elseif ($formulario === 'delete-anuidade') {
+        } 
+
+        elseif ($formulario === 'delete-anuidade') {
             $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
             
             try {
@@ -65,11 +65,47 @@
                 $stmt = $pdo->prepare($sql);
                 $stmt->bindParam(':id', $id, PDO::PARAM_INT);
                 $stmt->execute();
-                echo "Anuidade excluída com sucesso!";
             } catch (PDOException $e) {
                 echo "Erro ao excluir anuidade: " . $e->getMessage();
             }
         }
+        elseif ($formulario === 'editar-anuidade') {
+            $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
+            $ano = filter_input(INPUT_POST, 'ano', FILTER_SANITIZE_NUMBER_INT);
+            $valor = filter_input(INPUT_POST, 'valor', FILTER_SANITIZE_NUMBER_INT);
+            
+            try {
+                $sql = 'UPDATE "TecnoTech".anuidades SET ano = :ano, valor = :valor WHERE id = :id';
+                $stmt = $pdo->prepare($sql);
+                $stmt->bindParam(':ano', $ano);
+                $stmt->bindParam(':valor', $valor);
+                $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+                $stmt->execute();
+            } catch (PDOException $e) {
+                echo "Erro ao atualizar anuidade: " . $e->getMessage();
+            }
+        }
+        elseif ($formulario === 'editar-associado') {
+            $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
+            $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+            $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+            $cpf = filter_input(INPUT_POST, 'cpf', FILTER_SANITIZE_SPECIAL_CHARS);
+            $dataFiliacao = filter_input(INPUT_POST, 'dataFiliacao', FILTER_SANITIZE_SPECIAL_CHARS);
+        
+            try {
+                $sql = 'UPDATE "TecnoTech".associados SET nome = :nome, email = :email, cpf = :cpf, "data-filiacao" = :dataFiliacao WHERE id = :id';
+                $stmt = $pdo->prepare($sql);
+                $stmt->bindParam(':nome', $nome);
+                $stmt->bindParam(':email', $email);
+                $stmt->bindParam(':cpf', $cpf);
+                $stmt->bindParam(':dataFiliacao', $dataFiliacao);
+                $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+                $stmt->execute();
+            } catch (PDOException $e) {
+                echo "Erro ao atualizar associado: " . $e->getMessage();
+            }
+        }
+        
     }
 
     // Armazenando todos os associados em um array para exibi-los no HTML
@@ -99,6 +135,7 @@
 </head>
 <body>
     <main>
+        <h1 class="cabecalho">DEVS RN GESTÃO</h1>
         <section class="associados-section">
             <div class="titulo-section">
                 <h1>Associados</h1>
@@ -108,7 +145,7 @@
                 <?php foreach ($associadosArray as $associado): ?>
                     <li class="associado">
                         <h2><?php echo htmlspecialchars($associado['nome']); ?></h2>
-                        <img src="/static/img/edit-icon.svg">
+                        <img src="/static/img/edit-icon.svg" onclick="editAssociado(<?php echo $associado['id']; ?>, '<?php echo $associado['nome']; ?>', '<?php echo $associado['email']; ?>', '<?php echo $associado['cpf']; ?>', '<?php echo $associado['data-filiacao']; ?>')">
                         <img src="/static/img/search-icon.svg">
                         <img src="/static/img/delete-icon.svg" onclick="deleteAssociado(<?php echo $associado['id']; ?>)">
                     </li> 
@@ -132,7 +169,7 @@
                     <?php foreach ($anuidadesArray as $anuidade): ?>
                         <tr>
                             <td><?php echo htmlspecialchars($anuidade['ano']); ?></td>
-                            <td><?php echo "R$" . htmlspecialchars($anuidade['valor']); ?> <img src="/static/img/edit-icon.svg"> <img src="/static/img/delete-icon.svg" onclick="deleteAnuidade(<?php echo $anuidade['id']; ?>)"></td>
+                            <td><?php echo "R$" . htmlspecialchars($anuidade['valor']); ?> <img src="/static/img/edit-icon.svg" onclick="editAnuidade(<?php echo $anuidade['id']; ?>, '<?php echo $anuidade['ano']; ?>', '<?php echo $anuidade['valor']; ?>')"> <img src="/static/img/delete-icon.svg" onclick="deleteAnuidade(<?php echo $anuidade['id']; ?>)"></td>
                         </tr>
                     <?php endforeach; ?>
                     </tbody>
@@ -172,6 +209,38 @@
         </div>
     </div>
     
+    <div class="modal-editar-associados">
+        <div class="modal-container">
+            <div class="icone-fechar-modal-container"><img src="/static/img/close-icon.svg" class="btn-fechar-modal" id="fechar-modal-editar-associados"></div>
+            <h3>Editar Associado</h3>
+            <form action="index.php" method="POST">
+                <input type="hidden" name="formulario" value="editar-associado">
+                <input type="hidden" name="id" value="">
+                <div class="form-row">
+                    <div>
+                        <label>Nome</label>
+                        <input type="text" name="nome">
+                    </div>
+                    <div>
+                        <label>Email</label>
+                        <input type="email" name="email">
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div>
+                        <label>CPF</label>
+                        <input name="cpf">
+                    </div>
+                    <div>
+                        <label>Data de Filiação</label>
+                        <input type="date" name="dataFiliacao">
+                    </div>
+                </div>
+                <button>Editar</button>
+            </form>
+        </div>
+    </div>
+    
     <div class="modal-cadastro-anuidade">
         <div class="modal-container">
             <div class="icone-fechar-modal-container"><img src="/static/img/close-icon.svg" class="btn-fechar-modal" id="fechar-modal-anuidade"></div>
@@ -189,6 +258,28 @@
                     </div>
                 </div>
                 <button>Cadastrar</button>
+            </form>
+        </div>
+    </div>
+    
+    <div class="modal-edicao-anuidade">
+        <div class="modal-container">
+            <div class="icone-fechar-modal-container"><img src="/static/img/close-icon.svg" class="btn-fechar-modal icone-fechar-modal-anuidade"></div>
+            <h3>Editar Anuidade</h3>
+            <form action="index.php" method="POST">
+                <input type="hidden" name="formulario" value="editar-anuidade">
+                <input type="hidden" name="id" value="">
+                <div class="form-row">
+                    <div>
+                        <label>Ano</label>
+                        <input placeholder="Ex: 2024" name="ano">
+                    </div>
+                    <div>
+                        <label>Valor</label>
+                        <input placeholder="Ex: 200" name="valor">
+                    </div>
+                </div>
+                <button>Editar</button>
             </form>
         </div>
     </div>
--- static/js/script.js
+++ static/js/script.js
@@ -3,7 +3,11 @@ const btnFecharModalCadastroAssociados = document.querySelector(".btn-fechar-mod
 const modalCadastroAssociados = document.querySelector(".modal-cadastro-associados");
 const btnAbrirModalCadastroAnuidade = document.querySelector(".btn-adicionar-anuidade")
 const modalCadastroAnuidade = document.querySelector(".modal-cadastro-anuidade");
-const btnFecharModalCadastroAnuidade = document.querySelector("#fechar-modal-anuidade")
+const btnFecharModalCadastroAnuidade = document.querySelector("#fechar-modal-anuidade");
+const modalEdicaoAnuidade = document.querySelector(".modal-edicao-anuidade");
+const modalEdicaoAssociado = document.querySelector(".modal-editar-associados");
+const btnFecharModalEditarAssociado = document.querySelector("#fechar-modal-editar-associados");
+const btnFecharModalEditarAnuidade = document.querySelector(".icone-fechar-modal-anuidade");
 
 abrirModalCadastroAssociados.addEventListener("click", () => {
     modalCadastroAssociados.style.display = "flex";
@@ -21,6 +25,15 @@ btnFecharModalCadastroAnuidade.addEventListener("click", () => {
     modalCadastroAnuidade.style.display = "none";
 })
 
+btnFecharModalEditarAssociado.addEventListener("click", () => {
+    modalEdicaoAssociado.style.display = "none";
+})
+
+btnFecharModalEditarAnuidade.addEventListener("click", () => {
+    modalEdicaoAnuidade.style.display = "none";
+    console.log("fechando modal editar anuidade");
+})
+
 function deleteAssociado(id) {
     if (confirm("Tem certeza de que deseja excluir este associado?")) {
         fetch("index.php", {
@@ -33,7 +46,7 @@ function deleteAssociado(id) {
         .then(response => response.text())
         .then(data => {
             alert("Associado excluído com sucesso!");
-            location.reload(); // Recarrega a página para atualizar a lista
+            location.reload();
         })
         .catch(error => console.error("Erro ao excluir associado:", error));
     }
@@ -51,8 +64,33 @@ function deleteAnuidade(id) {
         .then(response => response.text())
         .then(data => {
             alert("Anuidade excluída com sucesso!");
-            location.reload(); // Recarrega a página para atualizar a lista
+            location.reload();
         })
         .catch(error => console.error("Erro ao excluir anuidade:", error));
     }
+}
+
+function editAnuidade(id, ano, valor) {
+    modalEdicaoAnuidade.style.display = 'flex';
+
+    // Preencher o formulário de edição de anuidade com os valores já existentes
+    modalEdicaoAnuidade.querySelector('input[name="ano"]').value = ano;
+    modalEdicaoAnuidade.querySelector('input[name="valor"]').value = valor;
+    
+    // Adicionar o ID da anuidade que vai ser editada
+    modalEdicaoAnuidade.querySelector('input[name="id"]').value = id;
+}
+
+function editAssociado(id, nome, email, cpf, dataFiliacao) {
+    modalEdicaoAssociado.style.display = 'flex';
+
+    // Preencher o formulário com os dados já existentes
+    modalEdicaoAssociado.querySelector('input[name="nome"]').value = nome;
+    modalEdicaoAssociado.querySelector('input[name="email"]').value = email;
+    modalEdicaoAssociado.querySelector('input[name="cpf"]').value = cpf;
+    modalEdicaoAssociado.querySelector('input[name="dataFiliacao"]').value = dataFiliacao;
+
+    // Adicionar o ID do associado que vai ser editado
+    modalEdicaoAssociado.querySelector('input[name="formulario"]').value = 'editar-associado';
+    modalEdicaoAssociado.querySelector('input[name="id"]').value = id;
 }
